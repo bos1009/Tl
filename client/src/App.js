f10026a553eeb9592b4fa2257a06a26c9835ab3e
bos1009 <bos1009@naver.com>
2024-02-02 05:10:10 +0900

    server 01

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
-//import Customer from './components/Customer';
-import './App.css';
 import Customer from './components/Customer';
+import './App.css';
+//import Customer from './components/Customer';
 
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -17,140 +17,17 @@ import { render } from '@testing-library/react';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowx: 'auto'
   },
   table: {
     minwidth: 1080
   },
   progress:{
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing(2)
   }
 })
 
-
-/*
-const customers = [{
-  'id': 1,
-  'image': 'https://source.unsplash.com/random/64x64',
-  'name': '홍길동',
-  'birthday': '760225',
-  'gender': '남자',
-  'job': '직장인'
-},
-{
-  'id': 2,
-  'image': 'https://source.unsplash.com/random/64x64',
-  'name': '김대중',
-  'birthday': '760225',
-  'gender': '남자',
-  'job': '국회의원'
-},
-{
-  'id': 3,
-  'image': 'https://source.unsplash.com/random/64x64',
-  'name': '이재명',
-  'birthday': '760225',
-  'gender': '남자',
-  'job': '당대표'
-},
-{
-  'id': 4,
-  'image': 'https://source.unsplash.com/random/64x64',
-  'name': '윤석열',
-  'birthday': '760225',
-  'gender': '남자',
-  'job': '대통령'
-}
-]
-*/
-/*
-class App extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-              {customers.map(c => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                )
-              })
-              }
-            </TableBody>
-          
-        </Table>
-      </Paper>
-    );
-  }
-}
-
-export default withStyles(styles)(App);
-*/
-
-
-/*
-const styles = theme => ({
-  root:{
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
-  },
-  table: {
-    minWidth: 1080
-  }
-
-
-})
-
-
-const customers = [
-  {
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/1',
-    'name' : '홍길동',
-    'birthday' : '760225',
-    'gender' : '남자',
-    'job' : '직장인',
-  },
-  {
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/2',
-    'name' : '이재명',
-    'birthday' : '630331',
-    'gender' : '남자',
-    'job' : '국회의원',
-  },
-  {
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/3',
-    'name' : '윤석열',
-    'birthday' : '660205',
-    'gender' : '남자',
-    'job' : '대통령',
-  }
-]
-*/
 
 class App extends Component {
   state = {
@@ -159,7 +36,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.timer = setInterval(this.progress, 20);
+    
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
--- server.js
+++ server.js
@@ -1,47 +1,36 @@
+
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = json.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://source.unsplash.com/random/64x64',
-            'name': '홍길동',
-            'birthday': '760225',
-            'gender': '남자',
-            'job': '직장인'
-        },
-        {
-            'id': 2,
-            'image': 'https://source.unsplash.com/random/64x64',
-            'name': '김대중',
-            'birthday': '660325',
-            'gender': '남자',
-            'job': '국회의원'
-        },
-        {
-            'id': 3,
-            'image': 'https://source.unsplash.com/random/64x64',
-            'name': '이재명',
-            'birthday': '680625',
-            'gender': '남자',
-            'job': '당대표'
-        },
-        {
-            'id': 4,
-            'image': 'https://source.unsplash.com/random/64x64',
-            'name': '윤석열',
-            'birthday': '701225',
-            'gender': '남자',
-            'job': '대통령'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-    ]);
-})
+    );
+    
+});
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
